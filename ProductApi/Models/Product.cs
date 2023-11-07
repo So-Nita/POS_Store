f56616b7e99e22183db39d06e6658172c75543f2
BACK-END.07 <BACK-END.07@TURBOTECH.COM>
2023-11-07 16:38:34 +0700

    Add Folder

---
+++ ProductApi/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace ProductApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public double Price { get; set; }
+        public string Thumbnail { get; set; } = null!;
+        public int Qty { get; set; }
+        public string Category { get; set; } = null!;
+    }
+    public class ApiResultTest
+    {
+        public List<Product> Products { get; set; } = new();
+    }
+}
